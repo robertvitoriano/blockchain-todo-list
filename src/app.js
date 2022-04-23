18b1d29b451f502a18b551313a4266f5836427ec
Robert Vitoriano <robertvitoriano@gmail.com>
2022-04-22 21:51:43 -0300

    connecting to ganache blockchain through wallet

---
+++ src/app.js
@@ -0,0 +1,71 @@
+const App = {
+  contracts:{},
+  load: async () =>{
+    //Load App...
+    await App.loadWeb3();
+    await App.loadAccount();
+    await App.loadContract();
+    await App.render();
+    console.log('App loading !!')
+  
+  },
+  loadWeb3: async () => {
+    if (typeof web3 !== 'undefined') {
+      App.web3Provider = web3.currentProvider
+      web3 = new Web3(web3.currentProvider)
+    } else {
+      window.alert("Please connect to Metamask.")
+    }
+    // Modern dapp browsers...
+    if (window.ethereum) {
+      window.web3 = new Web3(ethereum)
+      try {
+        // Request account access if needed
+        await ethereum.enable()
+        // Acccounts now exposed
+        web3.eth.sendTransaction({/* ... */})
+      } catch (error) {
+        // User denied account access...
+      }
+    }
+    // Legacy dapp browsers...
+    else if (window.web3) {
+      App.web3Provider = web3.currentProvider
+      window.web3 = new Web3(web3.currentProvider)
+      // Acccounts always exposed
+      web3.eth.sendTransaction({/* ... */})
+    }
+    // Non-dapp browsers...
+    else {
+      console.log('Non-Ethereum browser detected. You should consider trying MetaMask!')
+    }
+  },
+
+  loadAccount: async () =>{
+    const accounts = await web3.eth.getAccounts();
+    App.account = accounts[0];
+  },
+  loadContract: async () => {
+    const todoList = await $.getJSON('TodoList.json');
+    App.contracts.TodoList = TruffleContract(todoList);
+    App.contracts.TodoList.setProvider(App.web3Provider);
+    App.todoList = await App.contracts.TodoList.deployed();
+    console.log('Contract loaded !!')
+  },
+
+  render:async () =>{
+    const accountElement = document.querySelector('#account');
+    accountElement.innerHTML = "Account: "+App.account;
+    console.log({
+      element:accountElement,
+      account:App.account
+    })
+
+  }
+
+}
+
+//start the app when browser loads
+window.addEventListener('load', async () => {
+  await App.load()
+})
