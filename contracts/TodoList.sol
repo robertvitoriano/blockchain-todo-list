f1e67af4462b3c2c061c8c7a342eb88c93ac74d8
Robert Vitoriano <robertvitoriano@gmail.com>
2022-04-24 15:04:54 -0300

    add toggleCompleted function test and function

--- contracts/TodoList.sol
+++ contracts/TodoList.sol
@@ -18,6 +18,11 @@ contract TodoList {
    bool completed
  );
 
+   event TaskCompleted(
+    uint id,
+    bool completed
+  );
+
  constructor() {
    createTask("Check out this contract");
  }
@@ -26,7 +31,13 @@ contract TodoList {
    taskCount++;
    tasks[taskCount] =  Task(taskCount, _content, false);
    emit TaskCreated(taskCount, _content, false);
-
  }
+
+   function toggleCompleted(uint _id) public {
+    Task memory _task = tasks[_id];
+    _task.completed = !_task.completed;
+    tasks[_id] = _task;
+    emit TaskCompleted(_id, _task.completed);
+  }
 
 }
